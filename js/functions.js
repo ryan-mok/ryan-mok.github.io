2ce6b103cdc9f99d52be610f4c758d5ed191a7ce
Ryan Mok <ryan-mok@users.noreply.github.com>
2018-06-17 18:47:51 -0400

    Fix styling, add empty handling

--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,10 @@ window.init = function() {
   client = new ApiAi.ApiAiClient({accessToken: "16a9c2a5ff0b4dc58deb74a9c262ae3d"});
 };
 
+function welcomeEvent() {
+  return client.eventRequest('WELCOME');
+}
+
 function sendText(text) {
   return client.textRequest(text);
 }
--- js/layout.js
+++ js/layout.js
@@ -11,19 +11,20 @@
 	  resultDiv = document.getElementById("result");
   
 	  queryInput.addEventListener("keydown", queryInputKeyDown);
-		window.init()
+		window.init();
+		showWelcomeEvent();
 	}
   
 	function queryInputKeyDown(event) {
-	  if (event.which !== ENTER_KEY_CODE) {
+		var value = queryInput.value;
+		
+		if (event.which !== ENTER_KEY_CODE || value == "") {
 		return;
 	  }
   
-	  var value = queryInput.value;
 	  queryInput.value = "";
   
 	  createQueryNode(value);
-	  var responseNode = createResponseNode();
   
 	  sendText(value)
 		.then(function(response) {
@@ -32,33 +33,50 @@
 			result = response.result.fulfillment.speech
 		  } catch(error) {
 			result = "";
-		  }
+			}
+			var responseNode = createResponseNode();
 		  setResponseOnNode(result, responseNode);
 		})
 		.catch(function(err) {
-		  setResponseOnNode("Something goes wrong", responseNode);
+		  setResponseOnNode("Something went wrong. Please try again.", responseNode);
 		});
 	}
   
 	function createQueryNode(query) {
 	  var node = document.createElement('div');
-		node.className = "clearfix right-align right card-panel green accent-1";
+		node.className = "clearfix right-align right card-panel cyan accent-1";
 	  node.innerHTML = query;
 	  resultDiv.appendChild(node);
 	}
   
 	function createResponseNode() {
 	  var node = document.createElement('div');
-	  node.className = "clearfix left-align left card-panel text-darken-2";
-	  node.innerHTML = "...";
+	  node.className = "clearfix left-align left card-panel grey lighten-3";
+	  node.innerHTML = "Something went wrong. Please try again.";
 		resultDiv.appendChild(node);
 		window.scrollTo(0,document.body.scrollHeight);
 	  return node;
 	}
   
 	function setResponseOnNode(response, node) {
-	  node.innerHTML = response ? response : "[empty response]";
+	  node.innerHTML = response ? response : "Something went wrong. Please try again.";
 	  node.setAttribute('data-actual-response', response);
 	}
 
+	function showWelcomeEvent(event) {
+	  welcomeEvent()
+		.then(function(response) {
+		  var result;
+		  try {
+			result = response.result.fulfillment.speech
+		  } catch(error) {
+			result = "";
+			}
+			var responseNode = createResponseNode();
+		  setResponseOnNode(result, responseNode);
+		})
+		.catch(function(err) {
+		  setResponseOnNode("Something went wrong. Please try again.", responseNode);
+		});
+	}
   })();
